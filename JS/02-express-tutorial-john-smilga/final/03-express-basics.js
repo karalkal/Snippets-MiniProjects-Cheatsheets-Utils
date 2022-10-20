db243693e3f08df19624cc8eb1928c885748d6e9
karalkal <karalkal@yahoo.co.uk>
2022-10-20 23:44:50 +0100

    added first 2 basic express exercises i.e. get, all, use methods

--- JS/02-express-tutorial-john-smilga/final/03-express-basics.js
+++ JS/02-express-tutorial-john-smilga/final/03-express-basics.js
@@ -2,21 +2,23 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  console.log('user hit the resource')
-  res.status(200).send('Home Page')
+    console.log(`user hit url "${req.url}" with "${req.method}" method`)
+    res.send('Home Page')
 })
 
 app.get('/about', (req, res) => {
-  res.status(200).send('About Page')
+    console.log(`user hit url "${req.url}" with "${req.method}" method`)
+    res.send('About Page')
 })
 
 app.all('*', (req, res) => {
-  res.status(404).send('<h1>resource not found</h1>')
+    console.log(`user hit url "${req.url}" with "${req.method}" method`)
+    res.status(404)
+        .send('<h1>resource not found</h1>')
 })
 
-app.listen(5000, () => {
-  console.log('server is listening on port 5000...')
-})
+app.listen(5000, () => console.log('app listening on port 5000'))
+
 
 // app.get
 // app.post
--- JS/02-express-tutorial-john-smilga/final/04-express-app.js
+++ JS/02-express-tutorial-john-smilga/final/04-express-app.js
@@ -1,19 +1,24 @@
+/*
+This is the original file, plus some comments
+It won't work from this location because the routes to the files cannot be resolved from here
+In the original exercise the html and the public are at the same level
+The copy should be working though, locating the index and static from external/root dir
+*/
+
 const express = require('express')
 const path = require('path')
-
 const app = express()
 
-// setup static and middleware
+// created folder specifically for the static files but can read them from main app folder too
+// app.use(express.static('./navbar-app'))
 app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
 })
 
 app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
+    res.status(404).send("Ain't gonna happen")
 })
 
-app.listen(5000, () => {
-  console.log('server is listening on port 5000....')
-})
+app.listen(5000, () => console.log('server listening on port 5000'))
