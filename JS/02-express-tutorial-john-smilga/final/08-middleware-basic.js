ed3a840ee0ea2efd642529f4307a4dcb54a1fad3
karalkal <karalkal@yahoo.co.uk>
2022-11-07 13:31:21 +0000

    added middleware info example

--- JS/02-express-tutorial-john-smilga/final/08-middleware-basic.js
+++ JS/02-express-tutorial-john-smilga/final/08-middleware-basic.js
@@ -3,21 +3,29 @@ const app = express()
 
 //  req => middleware => res
 
+// If we require same functionality in a few routes, 
+// we create dedicated function and use it as middleware (logger in this case)
+// express AUTOMATICALLY PASSES req, res, next to our middleware function
+// ALWAYS pass next(), otherwise app is stuck here
+
 const logger = (req, res, next) => {
   const method = req.method
   const url = req.url
   const time = new Date().getFullYear()
-  console.log(method, url, time)
-  next()
+  console.log("Method:", method, "\nURL: ", url, "\nYear: ", time)
+
+  // res.send('Testing')  // program would just stop here
+  next()  // IS CRUCIAL
 }
 
 app.get('/', logger, (req, res) => {
-  res.send('Home')
-})
-app.get('/about', logger, (req, res) => {
-  res.send('About')
+  res.send('Home page')
 })
 
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
+app.get('/about', logger, (req, res) => {
+  res.send('About page')
 })
+
+app.listen(5000, () =>
+  console.log('Server active on port 5000')
+)
