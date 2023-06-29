4395125e689eb5f1fff3e54d6f81bd241ab92f6b
karalkal <karalkal@yahoo.co.uk>
2023-06-29 17:24:20 +0100

    moved John Smilga's Express.js to a separate repo

--- JS/01-node-tutorial-john-smilga/07-mind-grenade.js
+++
@@ -1,9 +0,0 @@
-const num1 = 5
-const num2 = 10
-
-function addValues() {
-  console.log(`  As soon as the module is imported, the function is invoked!
-  the sum is : ${num1 + num2}`)
-}
-
-addValues()
